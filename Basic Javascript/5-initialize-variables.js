c3e717692ee8b649aa1bb3fa44415ec884264452
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-08 17:12:48 +0800

    5-initialize-variables

---
+++ Basic Javascript/5-initialize-variables.js
@@ -0,0 +1,16 @@
+// It is common to initialize a variable to an initial value in the same line as it is declared.
+
+// Creates a new variable called myVar and assigns it an initial value of 0
+var myVar = 0;
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Define a variable 'a' with var and initialize it to a value of '9'.
+ */
+
+// Solution
+
+var a = 9;
