042cf607c79761ca10f669743beb6480dac1e4eb
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-08 17:16:00 +0800

    6-declare-string

---
+++ Basic Javascript/6-declare-string.js
@@ -0,0 +1,25 @@
+// Previously you used the following code to declare a variable:
+
+var myName;
+
+// But you can also declare a string variable like this:
+
+var anotherName = "your name"
+
+// "your name" is called a string literal. A string literal, or string, is a series
+// of zero or more characters enclosed in double quotes ('"')
+
+/**
+ * Exercise
+ * 
+ * Instructions: 
+ * 
+ * Create two new string variables: myFirstName and myLastName and assign them
+ * the values of your first and last name respectively
+ * 
+ */
+
+// Solution
+
+var myFirstName = "Carlos Alfonso"
+var myLastName = "Iñigo"
