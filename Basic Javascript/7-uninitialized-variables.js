49f46875548b3e4119de82df76f48bc99790b7d7
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-08 17:19:57 +0800

    7-uninitialized-variables

---
+++ Basic Javascript/7-uninitialized-variables.js
@@ -0,0 +1,21 @@
+/**
+ * When JavaScript variables are declared, they have an initial value of undefined.
+ * If you do a mathematical operation on an undefined variable your result will be NaN,
+ * which means "Not a Number". If you concatenate a string with an undefined variable, 
+ * you will get a string of undefined.
+ */
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Initialize three variables 'a', 'b', 'c' with the values of '5', '10', and "I am a " respectively
+ * so that they will not be undefined.
+ */
+
+// Solution
+
+var a = 5;
+var b = 10;
+var c = "I am a";
