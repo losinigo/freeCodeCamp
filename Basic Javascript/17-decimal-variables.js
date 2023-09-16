482ca09ebedc5a61d7857aa0168e208e572a310c
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 07:54:41 +0800

    17-decimal-number

---
+++ Basic Javascript/17-decimal-variables.js
@@ -0,0 +1,20 @@
+// We can store decimal numbers in variables too. Decimal numbers are sometimes referred to as floating point numbers or floats.
+
+/**
+ * Note: when you compute numbers, they are computed with finite precision. Operations using float points may lead to different results
+ * than desired outcome. If you are getting one of these results, open a topic on the the freeCodeCamp forum.
+ */
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Create a variable myDecimal and give it a decimal value with the fractional part.
+ * 
+ * const ourDecimal = 5.7;
+ * 
+ * // Only change code below this line
+ */
+
+const myDecimal = 1.1;
