9eafe70fef7b66c78c1009fe2242c6e35d00001e
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 07:46:03 +0800

    14-divide-number

---
+++ Basic Javascript/14-divide-number.js
@@ -0,0 +1,19 @@
+// We can also divide one number by another.
+
+// JavaScript uses the / symbol for division.
+
+// Example:
+
+const myVar = 16 / 2; // myVar now has the value 8.
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Change the 0 so that the quotient is equal to 2.
+ * 
+ * const quotient = 66 / 0; 
+ */
+
+const quotient = 66 / 33;
