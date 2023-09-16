ddc8e5e825e13c9bd07f62060cff488c5c0ad73e
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 07:41:37 +0800

    12-subtract-number

---
+++ Basic Javascript/12-subtract-number.js
@@ -0,0 +1,19 @@
+// We can also subtract one number from another.
+
+// JavaScript uses the - symbol for subtraction
+
+// Example:
+
+const myVar = 12 - 6; // myVar would have the value of 6.
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Change the code so the diference is 12.
+ * 
+ * const difference = 45 - 0;
+ */
+
+const difference = 45 - 33;
