bb81889359096cf4c199f0a129489f6f5ad03554
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 07:39:12 +0800

    11-number-variables

---
+++ Basic Javascript/11-number-variable.js
@@ -0,0 +1,21 @@
+// Number is a data type in JavaScript which represents numeric data.
+
+// Now let's try to add two numbers using JavaScript.
+
+// JavaScript uses the + symbol as an addition operator when placed between two numbers.
+
+// Example:
+
+const myVar = 5 + 10; // myVar now has the value 15
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Change the 0 so that the sum will be equal to 20.
+ * 
+ * const sum = 10 + 0;
+ */
+
+const sum = 10 + 10;
