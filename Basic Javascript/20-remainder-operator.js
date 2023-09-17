bb50b0dd924cc2f54578c2887852ed41fad62c00
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 08:13:39 +0800

    20-remainder-operator

---
+++ Basic Javascript/20-remainder-operator.js
@@ -0,0 +1,36 @@
+// The remainder operator % gives the remainder of the division of two numbers.
+
+// Example:
+
+/**
+ * 5 % 2 = 1
+ * 5 / 2 = 2 remainder 1
+ */
+
+// Usage
+
+/** In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2. Even
+ * numbers have a remainder of 0, while odd numbers a remainder of 1.
+ */
+
+// 17 % 2 = 1
+// 48 % 2 = 0
+
+// Note
+
+/** 
+ * The remainder operator is sometimes incorrectly referred to as the modulus operator. It is very similar to modulus, but does not work
+ * properly with negative numbers.
+ */
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Set remainder equal to the remainder of 11 divided by 3 using the remainder operator.
+ * 
+ * const remainder;
+ */
+
+const remainder = 11 % 3;
