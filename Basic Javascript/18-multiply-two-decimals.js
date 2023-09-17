cd79390f1e2c3e852c7fcdc5fe4e9402143dcfe5
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 08:06:44 +0800

    18-multiply-two-decimal

---
+++ Basic Javascript/18-multiply-two-decimals.js
@@ -0,0 +1,15 @@
+// In JavaScript, you can also perform calculations with decimal numbers, just like whole numbers.
+
+// Let's multiply two decimals together and get their product.
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Change the 0.0 so that product will equal 5.0.
+ * 
+ * const product = 2.0 * 0.0
+ */
+
+const product = 2.0 * 2.5;
