b211397665771c576d76598322f0ca163750f688
los.inigoph@gmail.com <los.inigoph@gmail.com>
2023-09-17 08:08:45 +0800

    19-divide-decimal

---
+++ Basic Javascript/19-divide-decimal.js
@@ -0,0 +1,16 @@
+// Now let's divide one decimal by another.
+
+// Change the 0.0 so that quotient will equal 2.2.
+
+/**
+ * Exercise
+ * 
+ * Instructions:
+ * 
+ * Change the 0.0 so that quotient will equal to 2.2.
+ * 
+ * const quotient = 0.0 / 2.0
+ *  
+ * */
+
+const quotient = 4.4 / 2.0
